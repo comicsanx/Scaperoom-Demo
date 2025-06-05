84b7b15088497b3d407228139e363f21bef0721d
naiomi25 <naiomidiaz.her@gmail.com>
2025-06-05 09:36:17 +0000

    base de datos terminada , para revisar

--- src/api/models.py
+++ src/api/models.py
@@ -1,7 +1,7 @@
-from datetime import datetime
+
 from typing import List
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, DateTime, Float, ForeignKey, Integer,Boolean
+from sqlalchemy import String, Float, ForeignKey,Boolean
 from sqlalchemy.orm import Mapped, mapped_column,relationship
 
 db = SQLAlchemy()
@@ -10,39 +10,43 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    username: Mapped[str] = mapped_column(String(120), nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
+    best_time :  Mapped[float] = mapped_column(Float, nullable=True)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
-    # game: Mapped['Game'] = relationship(back_populates = "user_game",uselist=False)
+    user_games: Mapped[List['GameSession']] = relationship(back_populates = "user")
   
 
     def serialize(self):
         return {
             "id": self.id,
-            'name' : self.name    
+            "name" : self.name    
         }
     
     
-class Game(db.Model):
-    __tablename__ = 'game'
+class GameSession(db.Model):
+    __tablename__ = 'game_sessions'
 
     id : Mapped[int] = mapped_column(primary_key=True)
-    # time_screen_1  : Mapped[float] = mapped_column(Float,nullable=False, default = 0)
-    # time_screen_2 : Mapped[float] = mapped_column(Float,nullable=False, default = 0)
-    # current_screen: Mapped[int] = mapped_column(nullable=False )
-  
-    # id_user : Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True) 
-    # user_game: Mapped['User'] = relationship(back_populates = "game")
-
-
-    def serialize(self):
-             
-             return {
-                  "id": self.id,
-               
-
-             }
+    status:  Mapped[str] = mapped_column(nullable=False)
+    acumuated_time : Mapped[float] = mapped_column(Float, nullable=True)
+    total_time :  Mapped[float] = mapped_column(Float, nullable=True)
+    current_level  :Mapped[int] = mapped_column(Float, nullable=False)
+    user_id : Mapped[int] = mapped_column(ForeignKey("users.id")) 
     
-class Session(db.Model):
-    __tablename__ = 'game'
+    
+    user: Mapped['User'] = relationship(back_populates = "user_games") 
+   
+
+
+def serialize(self):
+        
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "status": self.status,
+            "accumulated_time": self.accumulated_time,
+            "final_total_time": self.final_total_time,
+            "current_level": self.current_level
+        }
