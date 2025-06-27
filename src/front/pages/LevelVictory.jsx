0ce6eff80fc8c070e872eff48c3b1fb22f452694
Sandra Gómez <69921622+comicsanx@users.noreply.github.com>
2025-06-27 10:59:57 +0000

    update y delete - dashboard a medias y correccion timer

--- src/front/pages/LevelVictory.jsx
+++ src/front/pages/LevelVictory.jsx
@@ -8,8 +8,9 @@ export function LevelVictory() {
     return (
         <div>
             <h1>¡Te has pasado el nivel!</h1>
+            <button onClick={() => navigate(`/dashboard`)}>menu</button>
             <button onClick={() => navigate(`/level-2`)}>Continuar a nivel 2</button>
-            <button onClick={() => navigate(`/game-victory`)}>Continuar a pantalla victoria</button>
+            {/* <button onClick={() => navigate(`/game-victory`)}>Continuar a pantalla victoria</button> */}
         </div>
     );
 }
